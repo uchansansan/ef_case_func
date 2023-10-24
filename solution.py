4770cacf927448b9fe46badd7b15918a0fd6732a
uchansansan <uchansansan@gmail.com>
2023-10-24 11:24:50 +0700

    add document string, main

--- solution.py
+++ solution.py
@@ -2,6 +2,7 @@ import turtle
 
 
 def draw_hexagon(size):
+    '''function, that draw a single hexagon by size'''
     my_turtle = turtle.Turtle()
 
     for _ in range(6):
@@ -10,9 +11,14 @@ def draw_hexagon(size):
 
 
 def get_num_hexagons():
+    '''function, that ask and checks num of hexagons from user, then return that num'''
     num = int(input())
     if num >= 4 and num <= 20:
         return num
     else:
         print('Кол-во должно быть от 4 до 20')
         get_num_hexagons()
+
+
+if __name__ == '__main__':
+    draw_hexagon(10)
