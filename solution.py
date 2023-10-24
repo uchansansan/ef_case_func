a0e5a377c380c61d889ea25a8d121d2b570e2763
uchansansan <uchansansan@gmail.com>
2023-10-24 11:13:47 +0700

    add get num hexagon

--- solution.py
+++ solution.py
@@ -7,3 +7,12 @@ def draw_hexagon(size):
     for _ in range(6):
         my_turtle.forward(size)
         my_turtle.right(60)
+
+
+def get_num_hexagons():
+    num = int(input())
+    if num >= 4 and num <= 20:
+        return num
+    else:
+        print('Кол-во должно быть от 4 до 20')
+        get_num_hexagons()
